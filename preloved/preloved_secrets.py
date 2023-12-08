4da35bb478c9aff6b7f73ddb2d2cb418949aa16b
Rufelle Emmanuel Pactol <rufelleemmanuelpactol7@gmail.com>
2023-12-08 23:14:32 +0800

    Fixed container blobs

--- preloved/preloved_secrets.py
+++ preloved/preloved_secrets.py
@@ -3,7 +3,16 @@ PORT=3306
 USER='preloveddbadmin'
 PASSWORD='Appdev2023!'
 DATABASE='preloved'
-
+"""
 AZURE_ACCOUNT_NAME = 'preloved'
 AZURE_ACCOUNT_KEY = 'BlGpkOPW7LWDZ9ZdJjONu4Z1yQYqC2u5X5OzNkhQf1RNHL+y4gzPVjxq6ao5VrVwcf14DUWH7Il0+AStpaek4w=='
 AZURE_CONTAINER = 'preloved-blob'
+"""
+
+AZURE_CONTAINER = 'preloved'
+
+AZURE_ACCOUNT_NAME = 'prelovedstorage'
+
+AZURE_ACCOUNT_KEY_1 = 'hEzIfeCENF5OSew7tt4T/hDbrZrjITgGG33aSKjT98Qg5UjKz/4/gOrP6hTU0k9K+5Pe2jDAd5FP+AStnXPBjA=='
+
+AZURE_ACCOUNT_KEY_2 = 'prlLe5OAsLzUpN/DbnKUMOql4Fn8TGwbqWNozp/Z4GLprBY/sC9yGW6QogSXm+1zNZBHzth9NDpQ+AStktqZpw=='
