2c25f8a4f7861528ae0bfc6053e3ad7f96a01d6e
MarionEmmanuelBuhat <marionemmanuelbuhat@gmail.com>
2024-01-18 09:14:44 +0800

    updated gitignore

--- preloved_secrets.py
+++
@@ -1,9 +0,0 @@
-SERVER='preloved.mysql.database.azure.com'
-PORT=3306
-USER='preloveddbadmin'
-PASSWORD='Appdev2023!'
-DATABASE='preloved'
-
-AZURE_ACCOUNT_NAME = 'preloved'
-AZURE_ACCOUNT_KEY = 'BlGpkOPW7LWDZ9ZdJjONu4Z1yQYqC2u5X5OzNkhQf1RNHL+y4gzPVjxq6ao5VrVwcf14DUWH7Il0+AStpaek4w=='
-AZURE_CONTAINER = 'preloved-blob'
